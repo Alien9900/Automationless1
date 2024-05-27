8316ef7fabe01d5fcdeacffbf49f0ac600859c50
Alien9900 <156238799+Alien9900@users.noreply.github.com>
2024-05-27 14:57:09 +0300

    lesson_1_task_2.py

--- lesson_1_task_2.py
+++ lesson_1_task_2.py
@@ -1,5 +1,4 @@
-def my_age(age):
-    print("мой возраст сейчас", age)
-
-my_age(25)
-print("мой возраст через 3 года", 25 + 3)
+my_age = 25
+print("мой возраст сейчас", my_age)
+my_age += 3
+print("мой возраст через 3 года", my_age)
