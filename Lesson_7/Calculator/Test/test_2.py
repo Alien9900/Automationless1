eb98b09000c72e0c9bc2396db94d695b8a6c4ddb
Alien9900 <156238799+Alien9900@users.noreply.github.com>
2024-07-28 19:40:15 +0300

    Revert "Lesson 8"

--- Lesson_7/Calculator/Test/test_2.py
+++
@@ -1,8 +0,0 @@
-from Lesson_7.Calculator.Pages.calcmainpage import CalcMain
-
-
-def test_calculator_assert(chrome_browser):
-    calcmain = CalcMain(chrome_browser)
-    calcmain.insert_time()
-    calcmain.click_buttons()
-    assert "15" in calcmain.wait_button_txt()
