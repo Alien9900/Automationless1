19ca3c5f085aac2bce424bb2eef9a57da7805b78
Alien9900 <wwwfifastreet1@gmail.com>
2024-08-12 02:52:00 +0300

    add ninth homework

---
+++ Lesson_9/Pages/Database.py
@@ -0,0 +1,81 @@
+from sqlalchemy import create_engine, text
+
+class DataBase:
+    query = {
+        'create_company': text('insert into company (name, description) values (:name, :decription)'),
+        'max_company_id': text('select MAX(id) from company'),
+        'delete_company': text('delete from company where id = :company_id'),
+        'list_SELECT': text('select * from employee where company_id = :id'),
+        'item_SELECT': text('select * from employee where company_id = :c_id and id = :e_id'),
+        'maxID_SELECT': text('select MAX(id) from employee where company_id = :c_id'),
+        'item_DELETE': text('delete from employee where id = :id_delete'),
+        'item_UPDATE': text('update employee set first_name = :new name where id = :employer_id'),
+        'item_INSERT': text('insert into employee(company_id, first_name, last name, phone) values(:id, :name, :surname, :phone_num)')
+    }
+
+    def __init__(self, engine) -> None:
+        self.db = create_engine(engine)
+
+    def create_company(self, company_name: str, description: str):
+        with self.db.connect() as connection:
+            connection.execute(
+            self.query['create_company'],
+            parameters=dict(name=company_name, description=description)
+        )
+        connection.commit()
+        connection.close()
+    print("[INFO] Company created successfully")
+
+    def delete(self, company_id, int):
+        with self.db.connect() as connection:
+           connection.execute(self.query['delete_company'], parameters=dict(company_id=company_id))
+           connection.commit()
+           connection.close()
+        print("[INFO] DB connection closed")
+
+    def last_company_id(self):
+        with self.db.connect() as connection:
+            connection.execute(self.query['max_company_id']).fetchall()[0][0]
+            connection.commit()
+            connection.close()
+        print("[INFO] DB connection closed")
+
+    def get_list_employer(self, company_id: int):
+        with self.db.connect() as connection:
+            connection.execute(self.query['list_SELECT'], parameters=dict(id=company_id))
+            connection.commit()
+            connection.close()
+        print("[INFO] DB connection closed")
+
+    def create_employer(self, company_id: int, first_name: str, last_name: str, phone: str):
+        with self.db.connect() as connection:
+            connection.execute(self.query['item_INSERT'], parameters=dict(id=company_id, name=first_name, surname=last_name, phone_num=phone))
+            connection.commit()
+            connection.close()       
+        print("[INFO] DB connection closed")
+
+    def get_employer_id(self, company_id: int):
+        with self.db.connect() as connection:
+            connection.execute(self.query['maxID_SELECT'], parameters=dict(c_id=company_id))[0]
+            connection.commit()
+            connection.close()
+        print("[INFO] DB connection closed")    
+
+
+    def update_employer_info(self, new_name: str, id: int):
+        with self.db.connect() as connection:
+            connection.execute(self.query['item_UPDATE'], parameters=dict(new_name=new_name, employer_id=id))
+            connection.commit()
+            connection.close()
+        print("[INFO] DB connection closed")
+
+    def delete_employer(self, id: int):
+        with self.db.connect() as connection:
+            connection.execute(self.query['item_DELETE'], parameters=dict(id_delete=id))
+            connection.commit()
+            connection.close()
+        print("[INFO] DB connection closed")
+
+        
+            
+
---
+++ Lesson_9/Test/test_x_clients.py
@@ -0,0 +1,77 @@
+import pytest
+import json
+from Lesson_9.Pages.Employee import Employer
+from Lesson_9.Pages.Database import DataBase
+
+api = Employer("https://x-clients-be.onrender.com")
+db = DataBase("postgresql+psycopg2://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx")
+
+
+def test_get_list_employers():
+    # Создание компании
+    db.create_company("Vladislav QA", "SkyPro")
+    # Получение id последней созданной компании
+    max_id = db.last_company_id()
+    # Добавление сотрудника в компанию
+    db.create_employer(max_id, "Vladislav", "Ivashin", 80029396386)
+    # Получение списка сотрудников последней компании
+    db_employer_list = db.get_list_employer(max_id)
+    # API получение списка сотрудников последней созданной компании
+    api_employer_list = api.get_list(max_id)
+    # Сравнение списка сотрудников API и БД
+    assert len(api_employer_list) == len(db_employer_list)
+    # Удаляем сотрудника компании
+    response = (api.get_list(max_id))[0]
+    employer_id = response['id']
+    db.delete_employer(employer_id)
+    # Удаляем последнюю созданную компанию
+    db.delete(max_id)
+
+    
+
+def test_add_new_employer():
+    #создание и получение id последней созданной компании
+    db.create_company('Vladislav adding new employer', 'employer')
+    max_id = db.last_company_id()
+    # создание сотрудника 
+    db.create_employer(max_id, "Vladislav","Ivashin", 80029396386)
+    response = (api.get_list(max_id))[0]
+    employer_id = response['id']
+    #Cравнение id компании 
+    assert response['companyID'] == max_id
+    #Сравнение имени сотрудника с заданной ранее
+    assert response['firstName'] == "Vladislav"
+    #Удостоверяемся что статус сотрудника == true
+    assert response['isActive'] == True
+    #Сравнение фамилии сотрудника заданной ранее
+    assert response['lastName'] == "Ivashin"
+    # Удаление сотрудника
+    db.delete_employer(employer_id)
+    # Удаление компании
+    db.delete(max_id)
+
+
+def test_assertion_data():
+    db.create_company("Employer get id company", 'new')
+    max_id = db.last_company_id()
+    db.create_employer(max_id, "Vladislav", "Ivashin", 80029396386)
+    employer_id = db.get_employer_id(max_id)
+    get_api_info = (api.get_info(employer_id)).json()
+    assert get_api_info["firstName"] == "Vladislav"
+    assert get_api_info["lastName"] ==  "Ivashin"
+    assert get_api_info["phone"] ==  "80029396386"
+    db.delete_employer(employer_id)
+    db.delete(max_id)
+
+def test_update_user_info():
+    db.create_company('New updating company', 'test')
+    max_id = db.last_company_id()
+    db.create_employer(max_id, "Vladislav", "Ivashin", 80029396386 )
+    employer_id = db.get_employer_id(max_id)
+    db.update_employer_info("Alien", employer_id)
+    get_api_info = (api.get_info(employer_id)).json()
+    assert get_api_info["FirstName"] == "Alien"
+    assert get_api_info["lastName"] ==  "Ivashin"
+    assert get_api_info["isActive"] == True
+    db.delete_employer(employer_id)
+    db.delete(max_id)  
